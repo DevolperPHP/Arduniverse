2fe1cfde449c57a72c60e8170eb00652f77ce7d3
DevolperPHP <devolperphp@gmail.com>
2024-07-15 11:20:43 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+
+    username: {
+        type: String,
+    },
+
+    email: {
+        type: String,
+    },
+
+    phone: {
+        type: String,
+    },
+
+    password: {
+        type: String,
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+
+    cart: {
+        type: Array,
+        default: [],
+    },
+
+    isActive: {
+        type: Boolean,
+        default: false,
+    },
+
+    check: {
+        type: String,
+    },
+
+    Date: {
+        type: String,
+    },
+
+    sipher: {
+        type: String,
+    },
+
+    followers: {
+        type: Array,
+        default: [],
+    },
+
+    following: {
+        type: Array,
+        default: [],
+    },
+
+    bio: {
+        type: String,
+    },
+
+    posts: {
+        type: Array,
+        default: [],
+    },
+
+    profileImage: {
+        type: String,
+        default: "none",
+    },
+
+    notifications: {
+        type: Array,
+        default: [],
+    },
+
+    team: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+const User = mongoose.model('User', userSchema, 'User')
+module.exports = User
